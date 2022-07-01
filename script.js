ee65251dab4ea7203d296d76b2491be3792fc892
Maciej Godlewski <96136252+Machloy@users.noreply.github.com>
2022-07-01 13:43:32 +0200

    Change for arrow function whole script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 {
-  const biggerHeader = () => {
+  const headerChangerBtn = document.querySelector(".headerChanger__btn");
+
+  const resizeHeader = () => {
     const headerMain = document.querySelector(".js-header__main");
-    const headerChangerBtn = document.querySelector(".headerChanger__btn");
-    headerChangerBtn.addEventListener("click", () => {
-      headerMain.classList.toggle("header__main--bigger");
-      headerMain.classList.contains("header__main--bigger")
-        ? (headerChangerBtn.innerText = "Mniejszy nagłówek")
-        : (headerChangerBtn.innerText = "Większy nagłówek");
-    });
+
+    headerMain.classList.toggle("header__main--bigger");
+    headerChangerBtn.innerText = headerMain.classList.contains(
+      "header__main--bigger"
+    )
+      ? "Mniejszy nagłówek"
+      : "Większy nagłówek";
   };
-  biggerHeader();
+
+  const init = () => {
+    headerChangerBtn.addEventListener("click", resizeHeader);
+  };
+
+  init();
 }
